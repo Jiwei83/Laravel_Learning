8b561d399993c3c78463be58545cc10d1003063e
Jiwei <kricma83@gmail.com>
2017-08-31 12:33:48 +0800

    add send phone message, validate user input function and frontend display

--- OnlineBookStore/app/Tool/SMS/SendTemplateSMS.php
+++ OnlineBookStore/app/Tool/SMS/SendTemplateSMS.php
@@ -3,59 +3,57 @@
 namespace App\Tool\SMS;
 
 use App\Models\M3Result;
+include_once("CCPRestSDK.php");
+
 
 class SendTemplateSMS
 {
-  //主帐号
-  private $accountSid='aaf98f8949d575140149dd482efa04b5';
+    //主帐号
+    private $accountSid = '8aaf07085e2d97fd015e362e0b1802b0';
 
-  //主帐号Token
-  private $accountToken='c7a1c3563b1347f4adbe0734f981dac1';
+    //主帐号Token
+    private $accountToken = '0459f598a9054e9cade2a9ab17c6ef47';
 
-  //应用Id
-  private $appId='8a48b55149d5792d0149dd63a3c4046d';
+    //应用Id
+    private $appId = '8aaf07085e2d97fd015e362e0b6402b5';
 
-  //请求地址，格式如下，不需要写https://
-  private $serverIP='sandboxapp.cloopen.com';
+    //请求地址，格式如下，不需要写https://
+    private $serverIP = 'sandboxapp.cloopen.com';
 
-  //请求端口
-  private $serverPort='8883';
+    //请求端口
+    private $serverPort = '8883';
 
-  //REST版本号
-  private $softVersion='2013-12-26';
+    //REST版本号
+    private $softVersion = '2013-12-26';
 
-  /**
-    * 发送模板短信
-    * @param to 手机号码集合,用英文逗号分开
-    * @param datas 内容数据 格式为数组 例如：array('Marry','Alon')，如不需替换请填 null
-    * @param $tempId 模板Id
-    */
-  public function sendTemplateSMS($to,$datas,$tempId)
-  {
-       $m3_result = new M3Result;
+    /**
+     * 发送模板短信
+     * @param to 手机号码集合,用英文逗号分开
+     * @param datas 内容数据 格式为数组 例如：array('Marry','Alon')，如不需替换请填 null
+     * @param $tempId 模板Id
+     */
+    function sendTemplateSMS($to, $datas, $tempId)
+    {
+        // 初始化REST SDK
+        $m3_result = new M3Result;
+        $rest = new CCPRestSDK($this->serverIP, $this->serverPort, $this->softVersion);
+        $rest->setAccount($this->accountSid, $this->accountToken);
+        $rest->setAppId($this->appId);
 
-       // 初始化REST SDK
-       $rest = new CCPRestSDK($this->serverIP,$this->serverPort,$this->softVersion);
-       $rest->setAccount($this->accountSid,$this->accountToken);
-       $rest->setAppId($this->appId);
-
-       // 发送模板短信
-      //  echo "Sending TemplateSMS to $to <br/>";
-       $result = $rest->sendTemplateSMS($to,$datas,$tempId);
-       if($result == NULL ) {
-           $m3_result->status = 3;
-           $m3_result->message = 'result error!';
-       }
-       if($result->statusCode != 0) {
-           $m3_result->status = $result->statusCode;
-           $m3_result->message = $result->statusMsg;
-       }else{
-           $m3_result->status = 0;
-           $m3_result->message = '发送成功';
-       }
-
-       return $m3_result;
-  }
+        // 发送模板短信
+        $result = $rest->sendTemplateSMS($to, $datas, $tempId);
+        if ($result == NULL) {
+            $m3_result->status = 3;
+            $m3_result->message = 'Empty Result';
+        }
+        if ($result->statusCode != 0) {
+            $m3_result->status = $result->statusCode;
+            $m3_result->message = $result->statusMsg;
+        } else {
+            $m3_result->status = 0;
+            $m3_result->message = 'Succeed';
+        }
+        return $m3_result;
+    }
 }
-
 //sendTemplateSMS("18576437523", array(1234, 5), 1);
--- OnlineBookStore/routes/web.php
+++ OnlineBookStore/routes/web.php
@@ -84,7 +84,7 @@ Route::get('/register', 'View\MemberController@toRegister');
 
 Route::any('service/validate_code/create', 'Service\ValidateController@create');
 
-
+Route::any('service/validate_phone/send', 'Service\ValidateController@sendSMS');
 
 
 
